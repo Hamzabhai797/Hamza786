05cb0f708b8117d173e4f15227a80c67c311c076
Hamzabhai797 <119848548+Hamzabhai797@users.noreply.github.com>
2024-04-04 19:59:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,394 @@
+// Typescript Operators
+//  Arithmetic Operators:
+//  Addition (+)
+//  Subtraction (-)
+//  Multiplication (*)
+//  Division (/)
+//  Modulus (%)  yaha tak perhaya gaya
+//  Increment (++)
+//  Decrement (--)
+// Assignment Operators:
+// Assignment (=)
+// Addition assignment (+=)
+// Subtraction assignment (-=)
+// Multiplication assignment (*=)
+// Division assignment (/=)
+// Modulus assignment (%=)
+// Comparison Operators:
+// Equal to (==) type check nahi kary ga
+// Not equal to (!=)
+// Strict equal to (===)   type bhi check kary ga
+// Strict not equal to (!==)
+// Greater than (>)  bara
+// Greater than or equal to (>=)
+// Less than (<) chota
+// Less than or equal to (<=)
+// Logical Operators:
+// Logical AND (&&)  2no true honi chahy
+// Logical OR (||)   1 true honi chahy
+// Logical NOT (!)
+// Bitwise Operators:
+// Bitwise AND (&)
+// Bitwise OR (|)
+// Bitwise XOR (^)
+// Bitwise NOT (~)
+// Left shift (<<)
+// Right shift (>>)
+// Zero-fill right shift (>>>)
+// Unary Operators:
+// Unary plus (+)
+// Unary minus (-)
+// Logical NOT (!)
+// Bitwise NOT (~)
+// Typeof (typeof)
+// Void (void)
+// Delete (delete)
+// Conditional (Ternary) Operator:
+// Conditional (? : )
+// String Operators:
+// Concatenation (+)
+// Type Operators:
+// Instanceof (instanceof)
+// Type Assertion (as)
+// Nullish Coalescing Operator:
+// Nullish Coalescing Operator (??)
+/*let name1 = "Muhammad";
+console.log(name1);
+
+let name2 = "Israr";
+console.log(name2);
+
+const color = "Green";
+console.log(color);
+
+let my_name : string = "Zahoor"; //string
+let age : Number = 20; //Number
+let ismarried : boolean = false; //boolean
+let randomvalue : undefined = undefined; //undefined
+let newvalue : null = null; //null    current define nahi hua
+let (symbol & bigit)*/
+// var color = "Green";
+// console.log(color);
+// let myName = "Muhammad";
+// let myName1 = "Israr";
+// console.log(myName1);
+//console.log(5+3)
+// let num1 = 2000
+// let num2 = 522255225
+// console.log(num1+num2)
+/*let num1 = 53
+let num2 = 18
+console.log(53*18)*/
+/*let num1 = 45
+let num2 = 13
+console.log(num1-num2)*/
+/*let num1 = 45
+let num2 = 7
+console.log(num1/num2)*/
+/*let num1 = 35
+let num2 = 7
+console.log(num1%num2)*/
+/*let n = 7
+console.log(n)*/
+/*let stdName = "israr"
+let isSlot = "tuesday"
+let isAllowed = stdName === "israr" && isSlot === "tuesday"
+console.log(isAllowed)*/
+/*let email = "abc@gmail.com"
+let pass = "fb123"
+let iA = email === "israryaqoob600@gmail.com" && pass === "fb123"
+console.log(iA)*/
+/*let firstvalue = true
+let secondvalue = true
+let iA = firstvalue || secondvalue*/
+/*let firstvalue = "A"
+let secondvalue = "B"
+let iA = firstvalue === "A" || secondvalue ==="B"
+console.log(iA)*/
+// function square(){
+//     let x = 5
+//     console.log(x*x)
+// let stdName = "Hamza"
+// let isSlot = "Tuesday"
+// let isAllowed = stdName === 'Hamza' && isSlot === 'Tuesday'
+// console.log(isAllowed);
+// let sN = "Bilal"
+// let iS = "Tuesday"
+// let iA = sN === 'Hamza' && iS === 'Tuesday'
+// console.log(iA);                           false
+// let iA = true && true
+// console.log(iA);                     true
+// let iA = false && false
+// console.log(iA);                      false
+// let email = 'abc@gmail.com'
+// let pass = 'fb12345'
+// let iA = email === 'hamzanoor11111122@gmail.com' && pass === 'fb12345'
+// console.log(iA);                                            false
+// let email = 'abc@gmail.com'
+// let pass = 'fb12345'
+// let iA = email === 'abc@gmail.com' && pass === 'fb12345'
+// console.log(iA);                            true
+// let firstvalue = true
+// let secondvalue = true
+// let iA = firstvalue && secondvalue
+// console.log(iA);              true
+// let firstvalue = true
+// let secondvalue = true
+// let iA = firstvalue || secondvalue
+// console.log(iA); 
+// let firstvalue = "A"
+// let secondvalue = "b"
+// let iA = firstvalue === "A" || secondvalue === "B"
+// console.log(iA); 
+// console.log(!true);
+// if(condition){
+//     console.log("if wala block");
+// }
+// if (5 < 4) {
+//     console.log("if wala block");
+// }
+// else {
+//     console.log("else wala block");
+// }
+// let isPassed = true
+// if (isPassed){
+//     console.log("Bike mil gai");
+// }
+// else {
+//     console.log("saddar jany ka kitny huy");
+// }
+//  let isPassed = true
+// if (false){
+//     console.log("Bike mil gai");
+// }
+// else {
+//     console.log("saddar jany ka kitny huy");
+// }
+// let vehical = "car"
+// if(vehical === 'car'){
+//     console.log("car ki parking mai jao");
+// }
+// else if(vehical === 'bike'){
+//     console.log("bike ki parking ma jao");
+// }
+// else if (vehical === 'truck'){
+//     console.log("truck ki parking ma jao");
+// }
+// else if (vehical === 'bus'){
+//     console.log("bus ki parking ma jao");
+// }
+// else{
+//     console.log("Cycle ki parking ma jao");
+// }
+// let vehical = "car"
+// let vehcolor = "red"
+//  if(vehical === 'car'){
+//     if(vehcolor === 'red'){
+//         console.log("Right side waly parking ma jao");
+//     }
+//      console.log("car ki parking mai jao");
+//  }
+//  else if(vehical === 'bike'){
+//      console.log("bike ki parking ma jao");
+//  }
+//  else if (vehical === 'truck'){
+//      console.log("truck ki parking ma jao");
+//  }
+//  else if (vehical === 'bus'){
+//      console.log("bus ki parking ma jao");
+// }
+//  else{
+//      console.log("Cycle ki parking ma jao");
+//  }
+// nested if (If kai ander If)  ya else ka ander bhi karsakty hain
+//   Home Work (percentage) = 90
+// let percentage = 90
+// if (percentage === 90) {
+//     console.log("Congratulations! You have scored above 90.");
+// }
+// else if (percentage === 80 ){
+//     console.log("Zabardast you have score 80.");
+// }
+// else if (percentage === 60){
+//     console.log("you scored good marks.");
+// }
+// else if (percentage === 40){
+//     console.log("you are scored 40")
+// }
+// else{
+//     console.log("Oh ho! You are fail.");
+// }
+// let percentage = 95
+// if (percentage) = 90{
+//     console.log("grade A1")
+// }
+// let persontage = 80
+// else if (percentage = 80){
+//     console.log("grade A")
+// }
+// let persontage = 70
+// else if (percentage = 70){
+//     console.log("grade B")
+// }
+// let persontage = 50
+// else if (percentage = 50){
+//     console.log("grade C")
+// }
+// let persontage = 40
+// else if (percentage = 40){
+//     console.log("grade D")
+// }
+// else {
+//     console.log ("fail hoo gay ");
+// }
+// let boxA = true;
+// if(boxA === "false"){
+//     console.log{"success"};
+// }else if(boxA === true){
+//     console.log("success")
+// }
+// class 5
+// let month = 'Ramadan';
+//  console.log(month=='Ramadan');
+// if(month=='Ramadan'){
+//     console.log("No water bottle allow in ramadan");
+// }
+// let month = 'Muharram';
+//  console.log(month=='Ramadan');
+// if(month=='Ramadan'){
+//      console.log("No water bottle allow in ramadan");
+// }
+// if(month=='Muharram'){
+//     console.log("water bottle is allow ");
+// }
+// let month = 'Ramadan'
+// if(month=="Muharram"){
+//     console.log("Water bottle is allowed")
+// }
+// else{
+//     if(month=="Ramadan"){console.log("water is not allowed");}
+//     console.log("water bottle is allowed")
+// }
+//  let month = 'Ramadan'
+//  if(month=="Muharram"){
+//      console.log("Water bottle is allowed")
+//  }
+//  else{
+//      if(month=="Ramadan"){console.log("water is not allowed");}
+//      else{
+//          console.log("water bottle is allowed")
+//      }
+//  }
+// let month = 'March'
+// if(month =='January'){
+//     console.log("Drinking is Allowed")
+// }
+// else {
+//     if(month =="March"){console.log("Drinking is not allowed");}
+//     else{
+//         console.log("drinking is allowed")
+//     }
+// }
+// let month ='March'
+// if(month <='January'){
+//     console.log("Drinking is Allowed")
+// }
+// else {
+//     if(month >="March"){console.log("Drinking is not allowed");}
+//     else{
+//         console.log("drinking is allowed")
+//     }
+// }
+// function HalfFry(){
+//     // Egg + butter + salt
+//     let cocked = 1 + 1.5 + 2
+//     console.log(cocked, "Cocked")
+// }
+// let response = HalfFry()  // invoked
+// console.log(response, "Response")
+// function HalfFry(){
+//     // Egg + butter + salt
+//     let cocked = 1 + 1.5 + 2
+//    return cocked;
+// }
+// let response = HalfFry()  // invoked
+// console.log(response, "Response")
+// function classFunc(){
+//     console.log("Today we will learn functions");
+//     let simplefunc = "we will learn simple function"
+//     console.log(simplefunc)
+// }
+// let response = classFunc()
+// console.log(response)
+// class 6
+// function aik peace of code hota hai
+// function square(){
+//  let x = 5
+//  console.log(x*x)
+//  }
+//  square()
+//  square()
+//  square()
+//  square()
+// function square(){
+//     let x = 5
+//     console.log(x*x)
+//     }
+//     console.log("after");
+//     square()
+//     console.log("before");
+//     square()
+//     square()
+//     square()
+//   Dynamic function Ka liay parameters use hoty ha
+// function square(num:number){  // num : parameter     {} curly breaket  Ma jo bhi hoga usko block of code kahengy
+// console.log(num*num)
+// }
+// square(9);  // 9 : argument      argument pass kiay jaty hai parameter recieve kiay jaty hai
+// square(8);
+// function areaofCircle(radius: number){
+//       let circleArea = 3.142*radius*radius
+//     console.log(circleArea)
+// }
+// areaofCircle(5)
+// function areaofCircle(radius: number){
+//     const pi = 3.142;
+//     let circleArea = pi*radius*radius;
+//   console.log(circleArea);
+// }
+// areaofCircle(5);
+// function cook(dish:string){
+//     console.log("making"  +" "+ dish +'...')
+// console.log(dish + " "+ "is ready");
+// }
+// cook("Biryani")
+// function cook(dish:string){
+//     console.log("making"  +" "+ dish +'...');
+// console.log(dish + " "+ "is ready");
+// return dish +"khalo";
+// }
+// let cookedfood = cook("Biryani")
+// console.log(cookedfood)
+// function eat(dish:String){
+//     console.log("making" +" "+ dish +'...');
+//     console.log(dish + " "+ "is not ready");
+//     return dish +"Na khao"
+// }
+// let eatfood = eat("Nehari")
+// console.log(eatfood)
+// class 7
+//revision
+//node js is use to run javascript File
+// let a:string = "hello"
+// console.log(a)
+//a = "updated value"
+//a = 7
+//typecasting  
+//   what is concetenation
+//  console.log(5 >= 5 && "5"==="5")   true
+// console.log(5 < 5 && "5"==="5")  false
+// console.log(5 < 5 || "5"==="5")   true
+// console.log(6 != 8)   true
+var a = 6;
+var b = 5;
+console.log(!(6 = 5));
